81d45cab119d6f30b2abdf631f4fbf2c8f5745c9
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-06-20 19:26:12 -0400

    actualizo

--- telovendoM7/telovendoM7/settings.py
+++ telovendoM7/telovendoM7/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '!F5)Nn>6[@z|1zd}.g]`*[rGv75x`dO|G?R.P[M|C*o2v+Og,m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
