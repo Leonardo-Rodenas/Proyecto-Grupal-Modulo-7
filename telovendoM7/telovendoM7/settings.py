4e773d1de570c8a7b13b91a82299f327a99e909c
Leonardo Rodenas Escobar <leo.rodenas.e@gmail.com>
2023-06-21 19:33:50 -0400

    actualizo con implementacion futura de variables de entorno

--- telovendoM7/telovendoM7/settings.py
+++ telovendoM7/telovendoM7/settings.py
@@ -20,7 +20,7 @@ PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! 
 
 SECRET_KEY = ';NnYt$y1`FH5DCh{kzY0]]X*&]~)yVOwzEX{K(haMR+CWjtHQ1'
 
